c91e2a0064f3e1a1ecb6e871168decf9c06cc050
Daniela Baron <baron.daniela@gmail.com>
2013-12-06 13:02:21 -0500

    service and fix names in index.html

---
+++ client/app/scripts/services/AudioService.js
@@ -0,0 +1,35 @@
+'use strict';
+
+angular.module('flashlangApp')
+  .factory('AudioService', function ($document) {
+    
+    var audioElement = $document[0].createElement('audio'); 
+    var correctAnswerSound = "sounds/correct.mp3";
+		var incorrectAnswerSound = "sounds/incorrect.mp3";
+	  
+	  return {
+	    audioElement: audioElement,
+	    
+	    play: function(filename) {
+	        audioElement.src = filename;
+	        audioElement.play();     
+	  	},
+
+	  	playCorrect: function() {
+	  		this.play(correctAnswerSound);
+	  	},
+
+	  	playIncorrect: function() {
+	  		this.play(incorrectAnswerSound);
+	  	},
+
+	  	playFeedback: function(result) {
+	  		if (result) {
+					this.playCorrect();
+				} else {
+					this.playIncorrect();
+				}
+	  	}
+	  }
+
+  });
---
+++ client/app/scripts/services/GameService.js
@@ -0,0 +1,84 @@
+'use strict';
+
+angular.module('flashlangApp')
+	.factory('GameService', function() {
+
+		var scoreBoard = {};
+  	var cardIndex;
+  	var deck;
+
+  	var initScoreboard = function(deckToPlay, level) {
+  		scoreBoard.playedDate = new Date();
+  		scoreBoard.level = level;
+     	scoreBoard.deckName = deck.name
+     	scoreBoard.score = 0;
+     	scoreBoard.incorrectCards = [];
+     	scoreBoard.correctCards = [];
+     	scoreBoard.outOf = deck.cards.length;
+  	};
+
+  	var beginnerCheck = function(card, guess) {
+  		return (card.value === guess);
+  	};
+
+  	var advancedCheck = function(card, guess) {
+  		return (card.translated === guess);
+  	};
+
+  	var levelToCheckMap = {
+  		beginner: beginnerCheck,
+  		intermediate: advancedCheck,
+  		advanced: advancedCheck
+  	};
+
+		return {
+
+			initGame: function (deckToPlay, level) {
+      	deck = deckToPlay;
+       	cardIndex = 0;
+       	initScoreboard(deckToPlay, level);
+      },
+
+			getNextCard: function() {
+				if (cardIndex < deck.cards.length) {
+					var nextCard = deck.cards[cardIndex];
+					cardIndex = cardIndex + 1;
+					return nextCard;
+				}
+				return null;
+			},
+
+			checkGuess: function(card, guess, level) {
+				var checkFunction = levelToCheckMap[level];
+				var checkResult = checkFunction.apply(null, [card, guess]);
+				if (checkResult) {
+					return true;
+				} else {
+					return false;
+				}
+			},
+
+			updateScoreBoard: function(guessResult, card) {
+				if (guessResult) {
+					scoreBoard.score += 1;
+					scoreBoard.correctCards.push(card);
+				} else {
+					scoreBoard.incorrectCards.push(card);
+				}
+				return scoreBoard;
+			},
+
+			getScoreBoard: function() {
+				return scoreBoard;
+			},
+
+			getScorePercentage : function() {
+				var correct = scoreBoard.correctCards.length;
+				var incorrect = scoreBoard.incorrectCards.length;
+				var totalCards = correct + incorrect;
+				var divide = totalCards / scoreBoard.outOf;
+   			return Math.round((divide) * 100);
+			}
+
+		};
+	});
